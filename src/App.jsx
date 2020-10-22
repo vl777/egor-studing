bdbad553eab52549468e1ace321689c8eeb4ee32
Vadim Litvinov <litvinov.wk@gmail.com>
2020-10-22 22:59:50 +0300

    added todo component, need to fix bugs with array mapping, add logic on input and button,  and add delete button on every todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
 import React from "react";
 
 const App = () => {
+  let todo = [
+    {
+      text: "Выпить пива",
+      done: false,
+    },
+    {
+      text: "Поссать",
+      done: false,
+    },
+  ];
+
+  const onChangeCheckbox = (index) => {
+    return (todo[index].done = !todo[index].done);
+  };
   const onClick = () => {
-    alert("egor учись");
-    confirm(" Я учусь");
+    todo.push(inputValue);
+    console.log(todo);
   };
 
+  let inputValue = {
+    text: "",
+    done: false,
+  };
 
+  const onChange = (e) => (inputValue.text = e.target.value);
 
+  const Todo = (props) => {
+    console.log(todo);
+    return (
+      <div>
+        {props.text}
+        <input
+          type="checkbox"
+          value={props.done}
+          onChange={() => onChangeCheckbox(props.index)}
+        />
+        <button >Delete</button>
+      </div>
+    );
+  };
 
   return (
     <div>
-      
-      <button onClick={onClick}>push me</button>
+      {todo.map((todo, index) => (
+        <div key={index}>
+          <Todo content={todo} key={index} />
+        </div>
+      ))}
+      <div>
+        <input placeholder="Insert new todo" onChange={onChange} />
+        <button onClick={onClick}>Add todo</button>
+      </div>
     </div>
   );
 };
