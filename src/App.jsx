f5910f617c8e9e08775ce71b14d038965b191fa4
Vadim Litvinov <litvinov.wk@gmail.com>
2020-10-22 21:47:52 +0300

    clear the start project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  const onClick = () => {
+    alert("egor учись");
+  };
+
+
+
+
+  return (
+    <div>
+      
+      <button onClick={onClick}>push me</button>
+    </div>
+  );
+};
+
+export default App;
